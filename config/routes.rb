fef0b4c11f14db56306b2a20b0fbfd7660866806
Sasidhar <sasidharmuppala003@gmail.com>
2023-05-22 21:54:39 -0400

    Added new pages and created login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
-  get "/", to: "main#index"
+  root to: "main#index"
+  get "sign_up", to:"registration#new"
+  post "sign_up", to:"registration#create"
 end
